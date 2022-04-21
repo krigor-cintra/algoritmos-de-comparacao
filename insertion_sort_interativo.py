32c5fecc8dde705c954903cb9a2176a43152d99c
krigor-cintra <krigor.gcs@gmail.com>
2022-04-20 22:55:26 -0300

    Atualização 20/04

--- insertion_sort_interativo.py
+++ insertion_sort_interativo.py
@@ -1,1 +1,11 @@
-def insertion_sort_interativo():
+
+def insertion_sort(lista):
+    for i in range(1, len(lista)):
+        key = lista[i]
+        j = i - 1
+        while j >= 0 and key < lista[j]:
+            lista[j + 1] = lista[j]
+            j = j - 1
+        lista[j + 1] = key
+
+
